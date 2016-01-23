a568f8390f947ab238c9ad6e612c016a8e13d9fc
Felipe <fchiapini@gmail.com>
2016-01-23 11:46:27 -0200

    Added profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,7 @@
 class ProfilesController < ApplicationController
+	def new
+		# form where a user can fill out their own profile.
+		@user = User.find( params[:user_id] )
+		@profile = @user.build_profile
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  resource :users do
+  resources :users do
     resource :profile
   end
   resources :contacts
